ac202294e6ac234e8970533e4b74990a0c118dec
Abhinav prasad <abhinavprasad791@gmail.com>
2019-04-07 11:52:08 -0400

    pro

---
+++ pro7.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+        int ch;
+        printf("enter a value btw 1 to 2:");
+        scanf("%d",&ch);
+        switch(ch,ch+1)
+        {
+        case 1: printf("1\n");
+		break;
+        case 2: printf("2");
+  		break;    
+  }
+
+}
+
+
+
+
